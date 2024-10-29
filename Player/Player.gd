0f42747f9eab755d7eb8e1009075777c6e1b67ee
Glen Costigan <glen.costigan@gmail.com>
2024-10-29 06:16:49 -0400

    init

---
+++ Player/Player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+
+@export var SPEED := 200.0
+
+func get_y_direction() -> float:
+	return Input.get_action_strength("move_down") - Input.get_action_strength("up")
+	
+func _physics_process(delta: float) -> void:
+	var dir :Vector2=Vector2(0, get_y_direction())
+	velocity = dir * SPEED
+	
