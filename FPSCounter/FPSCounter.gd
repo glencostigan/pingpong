15ed07fcff14697441da8d3e015279dfb8664d0e
Glen Costigan <glen.costigan@gmail.com>
2024-10-29 14:42:32 -0400

    Add global FPS counter

---
+++ FPSCounter/FPSCounter.gd
@@ -0,0 +1,18 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var fps = Engine.get_frames_per_second()
+	text = "FPS: " + str(fps)
+
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("fps"):
+		print("fps pressed")
+		visible = !visible
