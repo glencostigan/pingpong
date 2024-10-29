646178932cb30d5b79fae95e0cfc6656d9a4fdad
Glen Costigan <glen.costigan@gmail.com>
2024-10-29 06:18:06 -0400

    run gdformat .

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 @export var speed = 750
-@export var direction = -1 
+@export var direction = -1
+
 
 func _physics_process(delta: float) -> void:
 	position += transform.x * direction * speed * delta
 
+
 func _on_body_entered(body: Node2D) -> void:
-	print('boom!')
+	print("boom!")
 	direction = direction * -1
 	#body.queue_free()
--- Main/Main.gd
+++ Main/Main.gd
@@ -3,8 +3,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print('Hello from main!')
-	pass # Replace with function body.
+	print("Hello from main!")
+	pass  # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
