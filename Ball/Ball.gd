7ca8aa59a4b0b6cc92dc51cbc6f7d3f9b40286ac
Glen Costigan <glen.costigan@gmail.com>
2024-10-29 13:29:41 -0400

    Add menu ui simple

--- Ball/Ball.gd
+++ Ball/Ball.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
-
 @export var SPEED = 20.0
+#@export var elasticity = 0.75
+
 
 func _ready() -> void:
 	velocity = Vector2(-SPEED, 0)
 
+
 func _physics_process(delta: float) -> void:
-	var collision :KinematicCollision2D = move_and_collide(velocity)
+	var collision: KinematicCollision2D = move_and_collide(velocity)
 	if collision:
 		var normal := collision.get_normal()
-		velocity = velocity.bounce(normal)
+		velocity = velocity.bounce(normal)  #* elasticity
--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
+var ball: CharacterBody2D
 
-var ball : CharacterBody2D
 
 func _ready() -> void:
 	ball = get_parent().get_node("Ball")
+
 
 func _physics_process(delta: float) -> void:
 	global_position.y = ball.global_position.y
