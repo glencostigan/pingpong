456c8b50ef99071e512c86bea24c490a29411e7c
Glen Costigan <glen.costigan@gmail.com>
2024-10-29 22:01:57 -0400

    Update with a few more changes

--- FPSCounter/FPSCounter.gd
+++ FPSCounter/FPSCounter.gd
@@ -16,3 +16,5 @@ func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fps"):
 		print("fps pressed")
 		visible = !visible
+	if Input.is_action_just_pressed("reset_scene"):
+		get_tree().reload_current_scene()
