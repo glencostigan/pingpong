caffeb5c64587c94c5b7c4765860cef1e5221bd6
Glen Costigan <glen.costigan@gmail.com>
2024-11-07 07:38:11 -0500

    Add speed for enemy and ball tracking

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,11 +1,15 @@
-extends CharacterBody2D
+extends RigidBody2D
 
 var ball: CharacterBody2D
-
+const SPEED = 30000
 
 func _ready() -> void:
 	ball = get_parent().get_node("Ball")
 
 
-func _physics_process(delta: float) -> void:
-	global_position.y = ball.global_position.y
+func _physics_process(delta):
+	# calculate the direction towards the ball
+	var direction = (ball.position - position).normalized()
+	
+	# move the paddle towards the ball
+	linear_velocity.y = direction.y * SPEED * delta
