2ebe738f2e1e7e6403464cbfc37241372f65ab43
Glen Costigan <glen.costigan@gmail.com>
2024-11-07 07:17:46 -0500

    fix unused var

--- FPSCounter/FPSCounter.gd
+++ FPSCounter/FPSCounter.gd
@@ -7,12 +7,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	var fps = Engine.get_frames_per_second()
 	text = "FPS: " + str(fps)
 
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("fps"):
 		print("fps pressed")
 		visible = !visible
