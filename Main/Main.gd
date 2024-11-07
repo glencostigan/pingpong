af72de931581a9c9db6b86dc219cb635e532b968
Glen Costigan <glen.costigan@gmail.com>
2024-11-07 07:15:06 -0500

    fix unused var

--- Main/Main.gd
+++ Main/Main.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	pass
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("reset_scene"):
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed("quit"):
